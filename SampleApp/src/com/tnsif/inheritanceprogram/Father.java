28621fee277e3780f4d41f14e291b271f28ed116
MamathaCoder <mamathawork123@gmail.com>
2026-06-02 23:48:49 +0530

    5th commit

---
+++ SampleApp/src/com/tnsif/inheritanceprogram/Father.java
@@ -0,0 +1,19 @@
+package com.tnsif.inheritanceprogram;
+
+public class Father {
+	//demo for inheritance
+	        int money=90000;
+	        String car="bmw";
+	        
+	        void drinking() {
+	                System.out.println("coffe");
+	        }
+	}
+	class Son extends Father{
+	        String cycle="pink";
+	        
+	        void read() {
+	                System.out.println("java");
+	        }
+	}
+	
